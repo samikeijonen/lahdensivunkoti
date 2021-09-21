a5d8bd6baaeb88d4b3b983e03de120d9394121d9
Sami Keijonen <sami.keijonen@foxnet.fi>
2021-09-21 21:26:31 +0300

    Fix wrapper.

--- htdocs/wp-content/themes/lahdensivunkoti/footer.php
+++ htdocs/wp-content/themes/lahdensivunkoti/footer.php
@@ -6,31 +6,30 @@
  */
 
 ?>
-</div>
-	</main><!-- .site-content -->
+	</div>
+		</main><!-- .site-content -->
 
-	<footer class="site-footer x-padding">
-		<div class="site-footer__container container grid has-2-columns alignfull">
-			<div class="site-footer__text top-margin">
-				<?php
-					// Echo from reusable blocks `/wp-admin/edit.php?post_type=wp_block`.
-					// Needs to have slug `alapalkki`.
-					$footer_post = get_page_by_path('alapalkki', '', 'wp_block');
-					if ( $footer_post ) :
-						echo apply_filters( 'the_content', get_the_content( null, false, absint( $footer_post->ID )) ); // phpcs:ignore
-					endif;
-				?>
+		<footer class="site-footer x-padding">
+			<div class="site-footer__container container grid has-2-columns alignfull">
+				<div class="site-footer__text top-margin">
+					<?php
+						// Echo from reusable blocks `/wp-admin/edit.php?post_type=wp_block`.
+						// Needs to have slug `alapalkki`.
+						$footer_post = get_page_by_path('alapalkki', '', 'wp_block');
+						if ( $footer_post ) :
+							echo apply_filters( 'the_content', get_the_content( null, false, absint( $footer_post->ID )) ); // phpcs:ignore
+						endif;
+					?>
+				</div>
+
+				<?php get_template_part( 'partials/global/social-links-nav' ); ?>
+
+				<div class="site-footer__logo-nav top-margin">
+					<?php get_template_part( 'partials/global/footer-nav' ); ?>
+				</div>
 			</div>
-
-			<?php get_template_part( 'partials/global/social-links-nav' ); ?>
-
-			<div class="site-footer__logo-nav top-margin">
-				<?php get_template_part( 'partials/global/footer-nav' ); ?>
-			</div>
-		</div>
-	</footer>
-
+		</footer>
+	</div>
 	<?php wp_footer(); ?>
-
 </body>
 </html>
--- htdocs/wp-content/themes/lahdensivunkoti/header.php
+++ htdocs/wp-content/themes/lahdensivunkoti/header.php
@@ -8,24 +8,24 @@
 get_template_part( 'partials/header/head' );
 ?>
 
-<body <?php body_class(); ?>>
+<body <?php body_class( 'site' ); ?>>
 	<?php wp_body_open(); ?>
+	<div class="site__wrapper">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'lahdensivunkoti' ); ?></a>
 
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'lahdensivunkoti' ); ?></a>
+		<header class="site-header x-padding">
+			<div class="site-header__container container alignfull">
 
-	<header class="site-header x-padding">
-		<div class="site-header__container container alignfull">
+				<div class="site-header__title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php bloginfo( 'name' ); ?>
+					</a>
+				</div>
 
-			<div class="site-header__title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-					<?php bloginfo( 'name' ); ?>
-				</a>
+				<?php get_template_part( 'partials/global/site-nav' ); ?>
+
 			</div>
+		</header>
 
-			<?php get_template_part( 'partials/global/site-nav' ); ?>
-
-		</div>
-	</header>
-
-	<main id="content" class="site-main x-padding">
-		<div class="site-main__container container">
+		<main id="content" class="site-main x-padding">
+			<div class="site-main__container container">
